390f0972ca611fc8280ccadc76fbfa4545c54d32
sarbot <mail@sarbot.de>
2018-01-07 03:45:02 +0100

    views initialisiert

--- main/models.py
+++ main/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+import datetime as dt
 
-# Create your models here.
+
+
+class Country(models.Model):
+
+    name = models.CharField(max_length=250)
+    short = models.CharField(max_length=10)
+    lon = models.FloatField(default=0, null=True)
+    lat = models.FloatField(default=0, null=True) 
+    url = models.CharField(max_length=500, null=True)
+    def __str__(self):
+        return self.name
+
+
+class Tour(models.Model):
+
+    name = models.CharField(max_length=250)
+    alias = models.CharField(max_length=250)  # url-safe
+    date_start = models.CharField('Tour Started', auto_now_add=True)
+    date_end = models.CharField('Tour Finished', null=True)
+    countries = models.ManyToMany(Country, blank=True)
+    color = models.CharField(max_length=10)
+    length = models.FloatField(blank=True)
+    img = models.ImageField()
+    tourlog = models.TextField(blank=True)  # html or md content?
+    
+    
+    def __str__(self):
+        """
+        Diese Funktion wird automatisch von python/django aufgerufen um 
+        ein Objekt zu bennen.
+        """
+        return self.name
+    
+    def get_duration(self):
+        """
+        returns duration of the tour if start and finish date are set correct.
+        :return duration: <datetime.timedelta>
+        """
+        try:
+            duration = date_start - date_end
+        except:
+            duration = dt.timedelta(days=0)
+        return duration
+
+
+class Daylog(models.Model):
+    date = models.DateTimeField('Date of the Day', auto_now_add=True)
+    text = models.TextField(blank=True)
+    tour = models.ForeignKey(Tour, blank=True)
+    length = models.FloatField(blank=True)
+    uptime = models.FloatField(blank=True)
+    height = models.FloatField(blank=True)
+    def __str__(self):
+        return str(self.date)
+
+
+
+class User(models.Model):
+    name = models.CharField(max_length=250, null=True)
+    chatid = models.IntegerField()
+    date_active = models.DateTimeField('Last Activity', auto_now_add=True)
+    date_signed = models.DateTimeField('User Registred', auto_now_add=True)
+
--- main/views.py
+++ main/views.py
@@ -1,3 +1,104 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Tour
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    """
+    Liste von allen Touren, bzw. neuste Tour und Links auf die Alten...
+    """
+    welcome_msg = 'Hallo'
+    latest_tour = Tour.objects.order_by('date_start').reverse()[0].id
+    context = {'text': welcome_msg, 'tour_id': latest_tour}
+    return render(request, 'main/home.html', context)
+
+
+def tour(requeust, tour):
+    """
+    Kompaktansicht einer Tour. Gesamtstrecke, links zu den jeweiligen Views, 
+    Zusammenfassung
+    """
+    context = {}
+    return render(request, 'main/tour.html', context)
+
+
+def map(request, tour):
+    """
+    Kartenansicht mit Fokus auf eine Bestimmte Tour
+    """
+    context = {}
+    return render(request, 'main/map.html', context)
+
+
+def logbook(request, tour):
+    """
+    Buch Ansicht: Das komplette logbuch mit ggf. links zu täglichen Bildern.
+    Alle Seiten auf einen Blick.
+    """
+    context = {}
+    return render(request, 'main/book.html', context)
+
+
+def gallery(request, tour):
+    """
+    Photogalleryansicht mit allen bildern zum durchklicken/wischen (lightning)
+    """
+    context = {}
+    return render(request, 'main/gallery.html', context)
+
+
+def day(request, tour, day):
+    """
+    Tagesansicht: Kombination von Tagebucheintrag mit Kartenauschnitt und allen
+    ausgewählten fotos von dem Tag
+    """
+    context = {}
+    return render(request, 'main/day.html', context)
+
+
+def download(request, tour, package):
+    """
+    Zu jeder Tour sollen download pakete als zip generiert werden: 
+    zB Alles (alle bilder, alle tracks, alle tagebücher, txt+pdf)
+    Kompakt (kompressed track, kompressed fav pics, tagebücher txt
+    bilder, bücher, track only jeweils dann alle formate
+    """
+    return
+
+
+
+# Als Beispiele aus nem anderen Projekt
+    
+def archive(request):
+    title = 'All Posts'
+    posts = Post.objects.all()
+    category = request.GET.get('category', False)
+    tag = request.GET.get('tag', False)
+    if category:
+        posts = posts.filter(category__name__iexact = category)
+        title = 'Category: ' + category
+    elif tag:
+        posts = posts.filter(tags__name__iexact = tag)
+        title = 'Tag: ' + tag
+    context = dict(title=title, posts=posts)
+    return render(request, 'blog/archive.html', context)
+
+def post(request, post_id): 
+    post = get_object_or_404(Post, pk=post_id)
+    context = {'post': post}
+    return render(request, 'blog/post.html', context)
+
+
+def download(request, post_id, ftype):
+    post = get_object_or_404(Post, pk=post_id)
+    if ftype == 'pdf':
+        html = post.content_html()
+        pdf_file = pdfkit.from_string(html,False)
+        response = HttpResponse(pdf_file, content_type="text/pdf")
+        response["Content-Disposition"] = "attachment; filename=" + post.get_fname() + ".pdf"
+    elif ftype == 'md':
+        markdown = post.get_md()
+        response = HttpResponse(markdown, content_type="text/plain")
+        response["Content-Disposition"] = "attachment; filename="+ post.get_fname()+".md"
+    return response
+
