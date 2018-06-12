805ecaa8f8647a4ea5535488238f3d87ce97633a
sarbot <mail@sarbot.de>
2018-06-12 23:53:30 +0200

    more fixes

--- main/models.py
+++ main/models.py
@@ -19,7 +19,7 @@ class Tour(models.Model):
     name = models.CharField(max_length=250)
     alias = models.CharField(max_length=250)  # url-safe
     date_start = models.DateField('Tour Started', auto_now=True)
-    date_end = models.CharField('Tour Finished', null=True)
+    #date_end = models.CharField('Tour Finished', null=True)
     #countries = models.ManyToMany(Country, blank=True)
     #color = models.CharField(max_length=10)
     #length = models.FloatField(blank=True)
