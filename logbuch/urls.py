b72db67e1907dec5a7dfa643273962ba475eeb12
sarbot <mail@sarbot.de>
2018-06-12 23:36:23 +0200

    templates views apps

---
+++ logbuch/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import path
+from logbuch import views
+
+urlpatterns = [
+    path('tag/<int:tagnummer>', views.tag, name='logbucheintrag'),
+    path('list/', views.list, name='logbuch')
+]
