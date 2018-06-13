2640b42784a1eb8051624a0559e19630a5b75560
sarbot <mail@sarbot.de>
2018-06-13 15:19:50 +0200

    auto push

--- telebot/models.py
+++ telebot/models.py
@@ -5,7 +5,7 @@ from main.models import Tour
 class User(models.Model):
     telegram_id = models.IntegerField(unique=True)
     name = models.CharField(null=True, blank=True, max_length=255)
-    tour = models.ForeignKey(Tour, blank=True, on_delete=models.SET_NULL)
+    tour = models.ForeignKey(Tour, blank=True, null=True, on_delete=models.SET_NULL)
     tag = models.IntegerField(null=True, blank=True)
     created = models.DateTimeField(auto_created=True, null=True)
     admin = models.BooleanField(default=False)
