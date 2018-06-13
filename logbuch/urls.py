8e81f68c7396dbd96a854575ff35d213e3422478
sarbot <mail@sarbot.de>
2018-06-13 10:14:37 +0200

    auto push

--- logbuch/urls.py
+++ logbuch/urls.py
@@ -4,4 +4,5 @@ from logbuch import views
 urlpatterns = [
     path('<touralias>/', views.list, name='logbuch'),
     path('<touralias>/<int:tagnummer>', views.tag, name='logbucheintrag'),
+    # comment
 ]
