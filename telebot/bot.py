806d57c80dce0d3d8b293c0f5e171f4a37f6d9f3
sarbot <mail@sarbot.de>
2018-06-13 15:02:57 +0200

    auto push

--- telebot/bot.py
+++ telebot/bot.py
@@ -5,8 +5,10 @@ import os
 import django
 os.environ["DJANGO_SETTINGS_MODULE"] = 'tour.settings'
 django.setup()
-from main.models import Tour
 from django.urls import reverse
+
+from main.models import Tour
+from telebot.models import User
 
 import logging
 import telegram  # pkg: python-telegram-bot
@@ -25,8 +27,27 @@ updater = Updater(token='498664396:AAGcMqVSxVvnqxgC_a0twCnuIclORUEHYgI')
 dispatcher = updater.dispatcher
 
 # ----- Some statics ------ #
-msg_start = "*Hi*,\n Benutze mich! \n Bin zwar noch in arbeit, aber trotzdem cool."
+msg_greets= "*Hi* {},\n Gut dass du fragst, "
+msg_start = "*Benutze mich!* \n Bin zwar noch in arbeit, aber trotzdem cool. \n Mit /menu kriegst du ne kleine 'grafische' Oberfläche. /befehle gibt dir die direkten Befehle, falls du weißt was du tust."
 
+
+# --------- Helper Functions -------- #
+def get_or_create_user(update):
+    """
+    Diese Funktion durchsucht die Datenbank nach einem user mit passender
+    chat_id, wenn keiner gefunden wurde, wird ein neuer erstellt in die DB
+    geschrieben und übergeben.
+    :param update: Nimmt ein telegram update objekt (für id und name)
+    :return user: User-Objekt das aus der Datenbank erstellt wurde
+    """
+    try:
+        chat_id = update.message.chat_id
+    except:
+        chat_id = update.callback_query.from_user.id
+
+    user, new = User.objects.get_or_create(telegram_id=chat_id)
+
+    return user, new
 
 
 
@@ -36,14 +57,53 @@ def start(bot, update):
     """ This Function is called on /start.
     Its telegrams style guide to serve some info and hints on this command
     """
-    bot.send_message(chat_id=update.message.chat_id, text=msg_start, parse_mode='Markdown')
+    user, new = get_or_create_user(update)
+    msg = msg_start
+    if new:
+        msg = msg_greets + msg_start
+
+    bot.send_message(chat_id=update.message.chat_id, text=msg, parse_mode='Markdown')
 
 
 
 def ui(bot, update):
+    user, new = get_or_create_user(update)
+    """
+    if user.tour:
+        ui_tour(bot, update)
+    """
+
+    msg = 'Wähle eine der folgenden Touren:'
+    touren = Tour.objects.all()
+    buttons = []
+    for tour in touren:
+        try:
+            btn = InlineKeyboardButton(tour.name,
+                                       callback_data='ui_tour;{}'.format(tour.alias)),
+            buttons.append(btn)
+        except:
+            logging.exception('Could not create Button for Tourlist')
+
+    buttons.append(InlineKeyboardButton('Neu erstellen'), callback_data='ui_new;')
+    keyboard = [buttons]
+    markup = InlineKeyboardMarkup(keyboard)
+    try:  # to get a messg_id and edit
+        msg_id = update.callback_query.message.message_id
+        bot.edit_message_text(
+            text=msg,
+            chat_id=user.telegram_id,
+            message_id=msg_id,
+            parse_mode='Markdown',
+            reply_markup=markup)
+    except:  # create new
+        bot.send_message(chat_id=user.telegram_id, text=msg, parse_mode='Markdown', reply_markup=markup)
+
+
+def ui_tour(bot, update):
 
     # create default keyboard (main menu)
-    msg = 'Aktuell ist Tour xy koratzien ausgewählt\n vlt. noch wieviele tage oder so'
+    user, new = get_or_create_user(update)
+    msg = 'Ausgewählte Tour: {}'.format()
     keyboard = [[
         InlineKeyboardButton('Tour bearbeiten', callback_data='ui_edit_tour'),
         InlineKeyboardButton('Tour wechseln', callback_data='ui_change_tour'),
@@ -66,7 +126,6 @@ def ui(bot, update):
             reply_markup=markup)
     except:  # create new
         bot.send_message(chat_id=usr.chat_id, text=cfg_txt, parse_mode='Markdown', reply_markup=markup)
-
 
 
 # ------ Button definitions --------- #
--- telebot/models.py
+++ telebot/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from main.models import Tour
 
 # Create your models here.
+class User(models.Model):
+    telegram_id = models.IntegerField(null=True, blank=True, unique=True)
+    name = models.CharField(null=True, blank=True, max_length=255)
+    tour = models.ForeignKey(Tour, null=True, blank=True, on_delete=models.SET_NULL)
+    tag = models.IntegerField(null=True, blank=True)
+    created = models.DateTimeField(auto_created=True)
+    admin = models.BooleanField(default=False)
+
