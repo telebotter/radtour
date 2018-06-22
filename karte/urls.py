b96dab5b8130dbeaf3c20efa881aaf8259fe2d20
sarbot <mail@sarbot.de>
2018-06-22 03:13:15 +0200

    auto push

--- karte/urls.py
+++ karte/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from karte import views
+from karte.models import Schlafplatz
+from djgeojson.views import GeoJSONLayerView
 
 urlpatterns = [
     path('', views.index, name='index_karte')
     # path('<touralias>/', views.tour, name='tour_karte'),
     # path('<touralias>/<int:tagnummer>', views.tag, name='schlafplatz'),
+    path('data.geojson', GeoJSONLayerView.as_view(model=Schlafplatz), name='data')
 ]
