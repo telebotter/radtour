2fc0418f8966f5205cb40258d165c8cbdeb1164f
sarbot <mail@sarbot.de>
2018-06-22 13:00:28 +0200

    auto push

--- bilder/urls.py
+++ bilder/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from karte import views
 
 urlpatterns = [
-    path('', views.index, name='index_karte')
+    path('', views.index, name='index_bilder'),
     # path('<touralias>/', views.tour, name='tour_karte'),
     # path('<touralias>/<int:tagnummer>', views.tag, name='schlafplatz'),
 ]
