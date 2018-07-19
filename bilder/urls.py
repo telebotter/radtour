305b0e6494f294c59de481ba8d5b572e2326c49f
sarbot <mail@sarbot.de>
2018-07-19 18:37:48 +0200

    bilder

--- bilder/urls.py
+++ bilder/urls.py
@@ -3,7 +3,7 @@ from bilder import views as bilder_views
 
 urlpatterns = [
     path('', bilder_views.index, name='index_bilder'),
-    path('<touralias>/', bilder_views.album, name='tour_bilder')
+    path('<touralias>/', bilder_views.index, name='tour_bilder')
     # path('<touralias>/', views.tour, name='tour_karte'),
     # path('<touralias>/<int:tagnummer>', views.tag, name='schlafplatz'),
 ]
