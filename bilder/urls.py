c802b6c23ea29d5d5d2db0f2380f8c7bf6d03510
sarbot <mail@sarbot.de>
2018-07-19 17:34:34 +0200

    bilder

--- bilder/urls.py
+++ bilder/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from bilder import views as bilder_views
 
 urlpatterns = [
-    path('', views.index, name='index_bilder'),
+    path('', bilder_views.index, name='index_bilder'),
     path('<touralias>/', bilder_views.album, name='tour_bilder')
     # path('<touralias>/', views.tour, name='tour_karte'),
     # path('<touralias>/<int:tagnummer>', views.tag, name='schlafplatz'),
