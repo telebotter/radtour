60a4313168d4301e97d0267c33a5c4cffd00173a
sarbot <mail@sarbot.de>
2018-07-23 20:14:53 +0200

    auto push

--- bilder/forms.py
+++ bilder/forms.py
@@ -3,6 +3,6 @@
 from django import forms
 
 class FilterForm(forms.Form):
-    tag_lukas = forms.CheckboxSelectMultiple(label='Lukas', default=False)
-    tag_renas = forms.CheckboxSelectMultiple(label='Renas', default=False)
-    tag_hendrik = forms.CheckboxSelectMultiple(label='Hendrik', default=False)
+    tag_lukas = forms.CheckboxSelectMultiple(name='Lukas', value=False)
+    tag_renas = forms.CheckboxSelectMultiple(name='Renas', default=False)
+    tag_hendrik = forms.CheckboxSelectMultiple(name='Hendrik', default=False)
