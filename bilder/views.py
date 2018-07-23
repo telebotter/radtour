85eb5c048d4b56e3f01ef4421329def9944fd45f
sarbot <mail@sarbot.de>
2018-07-23 19:58:55 +0200

    auto push

--- bilder/views.py
+++ bilder/views.py
@@ -8,14 +8,17 @@ from main.models import Tour
 def index(request):
     """All images overview"""
     # apply filter
-    tour_alias = request.GET.get('tour', default=None)
+    tour_alias = request.GET.get('tour', default=False)
     labels = request.GET.getlist('labels', default=False)
-    #if tour_alias:
-    tour = get_object_or_404(Tour, alias=tour_alias)
-    bilder = get_list_or_404(Bild, private=False, tour=tour)
-    #else:
-    #    bilder = get_list_or_404(Bild, private=False)
-    #if labels:
-    #    bilder = bilder.filter(labels__in=labels)
+    if tour_alias and labels:
+        tour = get_object_or_404(Tour, alias=tour_alias)
+        bilder = get_list_or_404(Bild, private=False, tour=tour, labels__in=labels)
+    elif tour_alias:
+        tour = get_object_or_404(Tour, alias=tour_alias)
+        bilder = get_list_or_404(Bild, private=False, tour=tour)
+    elif labels:
+        bilder = get_list_or_404(Bild, private=False, labels__in=labels)
+    else:
+        bilder = get_list_or_404(Bild, private=False)
     ctx= {'bilder': bilder}
     return render(request, 'bilder/index.html', context=ctx)
