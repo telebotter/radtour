ca1fe744444b9e0453877519f8eceea19897f278
sarbot <mail@sarbot.de>
2018-07-23 20:27:58 +0200

    auto push

--- bilder/forms.py
+++ bilder/forms.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from django import forms
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from bilder.models import Label
 
 class FilterForm(forms.Form):
-    OPTIONS = (
-        ("AUT", "Austria"),
-        ("DEU", "Germany"),
-        ("NLD", "Neitherlands"),
-    )
-    Countries = forms.MultipleChoiceField(
+    LABELS = get_list_or_404(Label, private=False)
+    OPTIONS = []
+    for lab in LABELS:
+        OPTIONS.append(lab.name)
+    Filter = forms.MultipleChoiceField(
         widget=forms.CheckboxSelectMultiple,
         choices=OPTIONS)
