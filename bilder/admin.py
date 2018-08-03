efc7cd85c429f066a02964d87de9bd2c494464fb
sarbot <mail@sarbot.de>
2018-08-04 01:44:10 +0200

    auto push

--- bilder/admin.py
+++ bilder/admin.py
@@ -6,11 +6,11 @@ from imagekit.admin import AdminThumbnail
 
 
 class BildAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'admin_thumbnail')
+    list_display = ('__str__', 'admin_thumbnail')  # for list view
     admin_thumbnail = AdminThumbnail(image_field='bild_thumb')
     admin_thumbnail.short_description = 'Bild'
     big_thumbnail = AdminThumbnail(image_field='bild_web')
-    readonly_fields = ['big_thumbnail']
+    readonly_fields = ['big_thumbnail']  # for detail view
 
 admin.site.register(Bild, BildAdmin)
 admin.site.register(Label)
