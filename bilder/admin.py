21c4b518759b13575aa27118a1977806a24f6c47
sarbot <mail@sarbot.de>
2018-08-04 01:34:33 +0200

    auto push

--- bilder/admin.py
+++ bilder/admin.py
@@ -4,9 +4,13 @@ from django.contrib import admin
 from bilder.models import Bild, Label
 from imagekit.admin import AdminThumbnail
 
+
+@register(Bild)
 class BildAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'admin_thumbnail')
     admin_thumbnail = AdminThumbnail(image_field='bild_thumb')
+    admin_thumbnail.short_description = 'Bild'
+    readonly_fields = ['admin_thumbnail']
 
 admin.site.register(Bild, BildAdmin)
 admin.site.register(Label)
