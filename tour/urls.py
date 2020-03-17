029f3a7966e8cab1c64b4be12045d68d8f809f70
sarbot <mail@sarbot.de>
2020-03-17 23:45:06 +0100

    clean up

--- tour/urls.py
+++ tour/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
+from django.conf import settings
 from django.conf.urls import url
 from django.urls import include, path
 from django.contrib import admin
@@ -27,3 +29,8 @@ urlpatterns = [
     path('markdownx/', include(mdurls)),
     url(r'^', include('django_telegrambot.urls')),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns= urlpatterns + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+    urlpatterns= urlpatterns + static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
